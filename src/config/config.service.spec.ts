3c832493e6739af759cbaf30a205ea611ce15e29
lpontes7 <lucas.pontes@csptecnologia.com>
2021-11-04 22:54:37 -0300

    Adicionando configService + logInterceptor + swagger

---
+++ src/config/config.service.spec.ts
@@ -0,0 +1,24 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { ConfigService } from './config.service';
+
+describe('ConfigService', () => {
+  let service: ConfigService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        ConfigService,
+        {
+          provide: String,
+          useValue: '',
+        },
+      ],
+    }).compile();
+
+    service = module.get<ConfigService>(ConfigService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+});
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,51 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+import { writeFile as writeFileCallback } from 'fs';
+import { promisify } from 'util';
+import { LoggingInterceptor } from './utils/logging.interceptor';
+import { ConfigService } from './config/config.service';
+
+const writeFile = promisify(writeFileCallback);
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors({
+    maxAge: 3600,
+    allowedHeaders: ['token-usuario', 'content-type'],
+    exposedHeaders: ['Content-Disposition'],
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      skipMissingProperties: true,
+    }),
+  );
+
+  app.useGlobalInterceptors(new LoggingInterceptor());
+
+  const options = new DocumentBuilder()
+    .setTitle('instagram-api-photos')
+    .setVersion('0.0.1')
+    .build();
+
+  const configService = app.get<ConfigService>(ConfigService);
+  const port = configService.get('API_PORTA');
+
+  const document = SwaggerModule.createDocument(app, options);
+
+  await writeFile('swagger.json', JSON.stringify(document));
+  try {
+    SwaggerModule.setup('api', app, document, {
+      customSiteTitle: 'instagram-api-photos',
+    });
+  } catch (e) {
+    console.log(e);
+  }
+
+  await app.listen(port);
 }
 bootstrap();
